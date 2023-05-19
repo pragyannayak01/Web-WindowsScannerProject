09efb0b462534125db86843e8798be7e4486e021
pragyannayak01 <120356532+pragyannayak01@users.noreply.github.com>
2023-05-19 17:09:36 +0530

    oopssssssss

---
+++ WinAppScanner/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System.Windows.Forms;
+namespace WinAppScanner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.showImages = new System.Windows.Forms.Button();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.cmbscannerlist = new System.Windows.Forms.ComboBox();
+            this.lblScanner = new System.Windows.Forms.Label();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.btnJPEG = new System.Windows.Forms.Button();
+            this.btnPDF = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // showImages
+            // 
+            this.showImages.Location = new System.Drawing.Point(242, 216);
+            this.showImages.Name = "showImages";
+            this.showImages.Size = new System.Drawing.Size(85, 23);
+            this.showImages.TabIndex = 5;
+            this.showImages.Text = "Show Images";
+            this.showImages.UseVisualStyleBackColor = true;
+            this.showImages.Visible = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.splitter1.Location = new System.Drawing.Point(0, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(1038, 3);
+            this.splitter1.TabIndex = 38;
+            this.splitter1.TabStop = false;
+            // 
+            // cmbscannerlist
+            // 
+            this.cmbscannerlist.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbscannerlist.FormattingEnabled = true;
+            this.cmbscannerlist.Location = new System.Drawing.Point(113, 34);
+            this.cmbscannerlist.Name = "cmbscannerlist";
+            this.cmbscannerlist.Size = new System.Drawing.Size(239, 23);
+            this.cmbscannerlist.TabIndex = 9;
+            // 
+            // lblScanner
+            // 
+            this.lblScanner.AutoSize = true;
+            this.lblScanner.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScanner.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblScanner.Location = new System.Drawing.Point(25, 37);
+            this.lblScanner.Name = "lblScanner";
+            this.lblScanner.Size = new System.Drawing.Size(65, 16);
+            this.lblScanner.TabIndex = 39;
+            this.lblScanner.Text = "Scanner";
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(12, 75);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(1014, 488);
+            this.pbImage.TabIndex = 40;
+            this.pbImage.TabStop = false;
+            // 
+            // btnJPEG
+            // 
+            this.btnJPEG.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnJPEG.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJPEG.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnJPEG.Location = new System.Drawing.Point(632, 588);
+            this.btnJPEG.Name = "btnJPEG";
+            this.btnJPEG.Size = new System.Drawing.Size(147, 45);
+            this.btnJPEG.TabIndex = 41;
+            this.btnJPEG.Text = "Save As JPEG";
+            this.btnJPEG.UseVisualStyleBackColor = false;
+            this.btnJPEG.Click += new System.EventHandler(this.btnJPEG_Click);
+            // 
+            // btnPDF
+            // 
+            this.btnPDF.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.btnPDF.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPDF.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnPDF.Location = new System.Drawing.Point(840, 588);
+            this.btnPDF.Name = "btnPDF";
+            this.btnPDF.Size = new System.Drawing.Size(163, 45);
+            this.btnPDF.TabIndex = 42;
+            this.btnPDF.Text = "Save As PDF";
+            this.btnPDF.UseVisualStyleBackColor = false;
+            this.btnPDF.Click += new System.EventHandler(this.btnPDF_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1038, 645);
+            this.Controls.Add(this.btnPDF);
+            this.Controls.Add(this.btnJPEG);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.lblScanner);
+            this.Controls.Add(this.cmbscannerlist);
+            this.Controls.Add(this.splitter1);
+            this.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home";
+            //this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.SizeChanged += new System.EventHandler(this.Home_SizeChanged);
+            //this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+
+
+        //camera
+        private System.Windows.Forms.Button showImages;
+        private Splitter splitter1;
+        private ComboBox cmbscannerlist;
+        private Label lblScanner;
+        private PictureBox pbImage;
+        private Button btnJPEG;
+        private Button btnPDF;
+    }
+}
+
---
+++ WinAppScanner/Form1.cs
@@ -0,0 +1,221 @@
+﻿using Fleck;
+using NTwain;
+using NTwain.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using WIATest;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static WIATest.WIAScanner;
+
+namespace WinAppScanner
+{
+    public partial class Form1 : Form
+    {
+        ImageCodecInfo _tiffCodecInfo;
+        TwainSession _twain;
+        bool _stopScan;
+        bool _loadingCaps;
+        List<IWebSocketConnection> allSockets;
+        WebSocketServer server;
+        public Form1()
+        {
+            InitializeComponent();
+            #region start soket
+            if (NTwain.PlatformInfo.Current.IsApp64Bit)
+            {
+                Text = Text + " (64bit)";
+            }
+            else
+            {
+                Text = Text + " (32bit)";
+            }
+            foreach (var enc in ImageCodecInfo.GetImageEncoders())
+            {
+                if (enc.MimeType == "image/tiff") { _tiffCodecInfo = enc; break; }
+            }
+
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+
+            //Get the IP Address value from webconfig
+            string IPAddress = "ws://" + System.Configuration.ConfigurationManager.AppSettings["IP"].ToString();
+
+            //Get the Port No  value from webconfig
+            var portno = System.Configuration.ConfigurationManager.AppSettings["port"];
+
+            allSockets = new List<IWebSocketConnection>();
+            server = new WebSocketServer(IPAddress + ":" + portno);
+
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    if (message == "1100")
+                    {
+                        this.Invoke(new Action(() =>
+                        {
+                            this.WindowState = FormWindowState.Maximized;
+                        }));
+                    }
+                };
+            });
+            #endregion
+            BindScanner();
+        }
+        //Bind Scanner
+        public void BindScanner()
+        {
+            List<DeviceClass> devices = WIAScanner.GetDevices();
+            cmbscannerlist.ValueMember = "DeviceId";
+            cmbscannerlist.DisplayMember = "DeviceName";
+            cmbscannerlist.DataSource = devices;
+        }
+        //Convert Image To PDF
+        public string ImagesToPdf(string imagepaths, string pdfpath)
+        {
+            iTextSharp.text.Rectangle pageSize = null;
+
+            using (var srcImage = new Bitmap(imagepaths.ToString()))
+            {
+                pageSize = new iTextSharp.text.Rectangle(0, 0, srcImage.Width, srcImage.Height);
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                var document = new iTextSharp.text.Document(pageSize, 0, 0, 0, 0);
+                iTextSharp.text.pdf.PdfWriter.GetInstance(document, ms).SetFullCompression();
+                document.Open();
+                var image = iTextSharp.text.Image.GetInstance(imagepaths.ToString());
+                document.Add(image);
+                document.Close();
+
+                File.WriteAllBytes(pdfpath + @"\" + DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".pdf", ms.ToArray());
+            }
+            return pdfpath + @"\" + DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".pdf";
+        }
+        private void Home_SizeChanged(object sender, EventArgs e)
+        {
+            int pheight = this.Size.Height - 153;
+            pbImage.Size = new Size(pheight - 150, pheight);
+        }
+        //Save As JPEG click event
+        private void btnJPEG_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmbscannerlist.SelectedItem == null)
+                {
+                    MessageBox.Show(" Select Scanner!! ");
+                    return;
+                }
+                else
+                {
+                    //get images from scanner
+                    List<System.Drawing.Image> images = WIAScanner.Scan((string)cmbscannerlist.SelectedValue);
+                    var enviroment = System.Environment.CurrentDirectory;
+                    //string projectDirectory = Directory.GetParent(enviroment).Parent.FullName + @"\ScanFiles";
+                    string projectDirectory = @"D:\ScannerApp\ScanFiles";
+                    string Imgpath = string.Empty;
+                    foreach (System.Drawing.Image image in images)
+                    {
+                        pbImage.Image = image;
+                        pbImage.Show();
+                        pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+                        Imgpath = projectDirectory + @"\" + DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".jpeg";
+                        //save scanned image into specific folder
+                        image.Save(Imgpath, ImageFormat.Jpeg);
+                    }
+                    if (!string.IsNullOrEmpty(Imgpath))
+                    {
+                        byte[] buff = System.IO.File.ReadAllBytes(Imgpath);
+                        foreach (var socket in allSockets.ToList())
+                        {
+                            socket.Send(buff);
+                        }
+                        this.Invoke(new Action(() =>
+                        {
+                            this.WindowState = FormWindowState.Minimized;
+                        }));
+                    }
+                    MessageBox.Show("Scan Completed.");
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+        }
+        //Save As PDF click event
+        private void btnPDF_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (cmbscannerlist.SelectedItem == null)
+                {
+                    MessageBox.Show(" Select Scanner!! ");
+                    return;
+                }
+                else
+                {
+                    //get images from scanner
+                    List<System.Drawing.Image> images = WIAScanner.Scan((string)cmbscannerlist.SelectedValue);
+                    var enviroment = System.Environment.CurrentDirectory;
+                    string projectDirectory = @"D:\ScannerApp\ScanFiles";
+                    string pdfpath = string.Empty;
+                    string ImagePath = string.Empty;
+                    foreach (System.Drawing.Image image in images)
+                    {
+                        pbImage.Image = image;
+                        pbImage.Show();
+                        pbImage.SizeMode = PictureBoxSizeMode.StretchImage;
+                        ImagePath = projectDirectory + @"\" + DateTime.Now.ToString("yyyy-MM-dd HHmmss") + ".jpeg";
+
+                        //save scanned image into specific folder
+                        image.Save(ImagePath, ImageFormat.Jpeg);
+                        pdfpath=ImagesToPdf(ImagePath, projectDirectory);
+                    }
+                    if (!string.IsNullOrEmpty(ImagePath))
+                    {
+                        byte[] buff = System.IO.File.ReadAllBytes(ImagePath);
+                        foreach (var socket in allSockets.ToList())
+                        {
+                            socket.Send(buff);
+                        }
+                        this.Invoke(new Action(() =>
+                        {
+                            this.WindowState = FormWindowState.Minimized;
+                        }));
+                    }
+                    MessageBox.Show("Scan Completed.");
+                }
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message);
+            }
+
+        }
+    }
+}
